cd4996cdc28a8ea61c6d2166f673db0c48d20e24
BRIGHT <BRIGHT@DESKTOP-ST51CUU>
2024-02-24 12:04:59 +0300

    ...

--- DbBaglanti/Form1.Designer.cs
+++ DbBaglanti/Form1.Designer.cs
@@ -30,41 +30,131 @@
         {
             comboBox1 = new ComboBox();
             button1 = new Button();
+            txtColor = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            txtPrice = new TextBox();
+            label3 = new Label();
+            txtStock = new TextBox();
+            label4 = new Label();
+            txtName = new TextBox();
             SuspendLayout();
             // 
             // comboBox1
             // 
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(245, 12);
+            comboBox1.Location = new Point(21, 12);
             comboBox1.Name = "comboBox1";
             comboBox1.Size = new Size(267, 23);
             comboBox1.TabIndex = 0;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
             // 
             // button1
             // 
-            button1.Location = new Point(245, 41);
+            button1.Location = new Point(21, 41);
             button1.Name = "button1";
             button1.Size = new Size(267, 23);
             button1.TabIndex = 1;
-            button1.Text = "button1";
+            button1.Text = "Ürün Getir";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
+            // 
+            // txtColor
+            // 
+            txtColor.Location = new Point(394, 53);
+            txtColor.Name = "txtColor";
+            txtColor.Size = new Size(100, 23);
+            txtColor.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(339, 56);
+            label1.Name = "label1";
+            label1.Size = new Size(37, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Rengi";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(339, 86);
+            label2.Name = "label2";
+            label2.Size = new Size(35, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Fiyatı";
+            // 
+            // txtPrice
+            // 
+            txtPrice.Location = new Point(394, 83);
+            txtPrice.Name = "txtPrice";
+            txtPrice.Size = new Size(100, 23);
+            txtPrice.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(339, 115);
+            label3.Name = "label3";
+            label3.Size = new Size(38, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Adedi";
+            // 
+            // txtStock
+            // 
+            txtStock.Location = new Point(394, 112);
+            txtStock.Name = "txtStock";
+            txtStock.Size = new Size(100, 23);
+            txtStock.TabIndex = 6;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(339, 27);
+            label4.Name = "label4";
+            label4.Size = new Size(25, 15);
+            label4.TabIndex = 9;
+            label4.Text = "Adı";
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(394, 24);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(100, 23);
+            txtName.TabIndex = 8;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(516, 167);
+            Controls.Add(label4);
+            Controls.Add(txtName);
+            Controls.Add(label3);
+            Controls.Add(txtStock);
+            Controls.Add(label2);
+            Controls.Add(txtPrice);
+            Controls.Add(label1);
+            Controls.Add(txtColor);
             Controls.Add(button1);
             Controls.Add(comboBox1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private ComboBox comboBox1;
         private Button button1;
+        private TextBox txtColor;
+        private Label label1;
+        private Label label2;
+        private TextBox txtPrice;
+        private Label label3;
+        private TextBox txtStock;
+        private Label label4;
+        private TextBox txtName;
     }
 }
--- DbBaglanti/Form1.cs
+++ DbBaglanti/Form1.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Data.SqlClient;
 
 namespace DbBaglanti
@@ -12,20 +13,65 @@ namespace DbBaglanti
         private void button1_Click(object sender, EventArgs e)
         {
             SqlConnection con = new SqlConnection("data source=.;initial catalog=AdventureWorks2019;integrated security=true");
-            SqlCommand cmd = new SqlCommand("select Name from Production.Product", con);
+            SqlCommand cmd = new SqlCommand("select ProductId, Name from Production.Product", con);
             con.Open();
 
 
             SqlDataReader dr = cmd.ExecuteReader();
-
-            while(dr.Read())
+            List<Product> productList = new List<Product>();
+            while (dr.Read())
             {
 
-                comboBox1.Items.Add(dr["Name"]);
+
+                productList.Add(new Product()
+                {
+
+                    Id = (int)dr["ProductId"],
+                    Name = dr["Name"].ToString(),
+                });
+
+
 
             }
+
             dr.Close();
             con.Close();
+
+            comboBox1.DataSource = productList;
+            comboBox1.DisplayMember = "Name";
+            comboBox1.ValueMember = "Id";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // ürün detaylarýný bu aþama da getireceðiz
+
+            if (comboBox1.SelectedValue != null)
+            {
+                if (comboBox1.SelectedValue is Product)
+                {
+
+                }
+                else
+                {
+                    int id = (int)comboBox1.SelectedValue;
+                    SqlConnection con = new SqlConnection("data source=.;initial catalog=AdventureWorks2019;integrated security=true");
+                    SqlCommand cmd = new SqlCommand("select Name,Color,ListPrice,SafetyStockLevel from Production.Product where ProductId=" + id.ToString(), con);
+                    con.Open();
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        txtColor.Text = dr["Color"].ToString();
+                        txtName.Text = dr["Name"].ToString();
+                        txtPrice.Text = dr["ListPrice"].ToString();
+                        txtStock.Text = dr["SafetyStockLevel"].ToString();
+                    }
+                    dr.Close();
+                    con.Close();
+                }
+
+            }
+
         }
     }
 }
